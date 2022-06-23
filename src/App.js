49150f28419637ac5879a6fa433b0f854858b0d5
GiAn <giancarlos.araya@erictel.com>
2022-06-22 22:32:10 -0500

    Camera testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
+import Divider from './components/Divider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <AppBar>
+            <Toolbar>Testing Native camera</Toolbar>
+          </AppBar>
+          <Box sx={{mt:8}}>
+          <div className="normal">
+            <p>Normal input type</p>
+            <input type="file"></input>
+          </div>
+          </Box>
+          <Divider/>
+          <Box>
+            <div className="normal">
+              <p>Only image</p>
+              <input type="file" accept="image/*"></input>
+            </div>
+          </Box>
+          <Divider/> 
+          <Box>
+            <div className="normal">
+              <p>Camera directly</p>
+              <input type="file" accept="image/*" capture></input>
+              <p>Camera directly v2</p>
+              <input type="file" accept="image/*" capture="camera"></input>
+            </div>
+          </Box>
+          <Divider/>      
+        </Box>
       </header>
-    </div>
+    </>
+
   );
 }
 
---
+++ src/components/Divider.jsx
@@ -0,0 +1,10 @@
+import './divider.css'
+
+const Divider = () => {
+  return(
+    <div className="divider"></div>
+  )
+  
+}
+
+export default Divider
